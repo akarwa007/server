8f1fa968a0d0c91c1a84d6015dd5c33b5ba0eb75
akarwa007 <Slow45anu>
2016-06-25 23:35:45 +0000

    left out files

---
+++ PokerServer/ClientView/CasinoView.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json;
+namespace Poker.Server.ClientView
+{
+    public class CasinoView
+    {
+        private List<TableView> _list = new List<TableView>();
+        private static CasinoView _instance = new CasinoView();
+        private CasinoView()
+        {
+            TableManager t = TableManager.Instance;
+            foreach (var x in t.GetRunningTables())
+            {
+                _list.Add(new TableView((Table)x));
+            }
+        }
+        public static CasinoView Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+        public List<TableView> ListOfTables
+        {
+            get
+            {
+                return _list;
+            }
+        }
+        public static string Serialize()
+        {
+            
+            String jsonString = JsonConvert.SerializeObject(CasinoView.Instance);
+            return jsonString;
+        }
+    }
+}
