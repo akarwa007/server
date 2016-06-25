69e2d719b07e9f7a75aecdd66e2772d583c7c5ce
akarwa007 <Slow45anu>
2016-06-25 23:07:27 +0000

    left out file

---
+++ Poker.Common/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace Poker.Common
+{
+    public delegate bool createplayer_callback(string username, string encrypted_pwd);
+
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum GameName
+    {
+        Texas_Holdem,
+        Omaha_Hi_Low,
+        Seven_Card_Stud
+    }
+     [JsonConverter(typeof(StringEnumConverter))]
+    public enum GameSubName
+    {
+        Limit_Holdem,
+        No_Limit_Holdem
+    }
+
+}
