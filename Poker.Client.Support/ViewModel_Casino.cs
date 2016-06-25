295bd7ff19bed7761172faa6e92ae67f9aeda641
akarwa007 <Slow45anu>
2016-06-25 23:29:58 +0000

    left out teh file

---
+++ Poker.Client.Support/ViewModel_Casino.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json;
+
+namespace Poker.Client.Support
+{
+    public class ViewModel_Casino
+    {
+        private List<ViewModel_Table> _list = new List<ViewModel_Table>();
+      
+        public ViewModel_Casino()
+        {
+        }
+     
+        public List<ViewModel_Table> ListOfTables
+        {
+            get
+            {
+                return _list;
+            }
+        }
+    }
+}
